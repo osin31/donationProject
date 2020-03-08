3f0cd47c49379ba5042f97d2788b7f2c67f699d8
osin31 <songjh31@naver.com>
2020-03-08 21:13:52 +0900

    Add files via upload
    first Commit

---
+++ donationProject/donation/src/main/java/com/project/donation/testDonationReq.java
@@ -0,0 +1,13 @@
+package com.project.donation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testDonationReq {
+
+	@RequestMapping("/")
+	public String testDonation() {
+		return "testData";
+	}
+}
