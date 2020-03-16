39f9f08311300aefda8e8bef12c350cb71865807
osin31 <songjh31@naver.com>
2020-03-16 11:00:19 +0900

    /* donation 경로 수정 */

--- donationProject/donation/src/main/java/com/project/donation/controller/testDonationReq.java
+++ donationProject/donation/src/main/java/com/project/donation/controller/testDonationReq.java
@@ -1,12 +1,14 @@
 package com.project.donation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/donation")
 public class testDonationReq {
 
-	@RequestMapping(value="/")
+	@GetMapping(value="/test")
 	public String testDonation() {
 		return "/test";
 	}
